3a7dd5c80ab693ffd923c9e873f6ed6fd29f4728
gruidae <gruidae_2012101151@tutanota.com>
2020-12-17 05:51:30 +0000

    add a Schedules controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'schedules/index'
+  get 'schedules/show'
+  get 'schedules/new'
+  get 'schedules/edit'
+  get 'schedules/create'
+  get 'schedules/update'
+  get 'schedules/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
